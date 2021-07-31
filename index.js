dd5061ca868e609a101d87ef93e297db1d8b9a3a
softrain <yurimplus@naver.com>
2021-08-01 02:55:23 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+const config = require("./config/key");
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://softrain:0428yr@boilerplate.j84yu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello world!"));
+
+app.post("/register", (req, res) => {
+  //회원가입할 때 필요한 정보를 client에서 가져오면 데이터베이스에 넣어줌
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
