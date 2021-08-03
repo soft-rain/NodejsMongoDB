1796b6e2484def84423b5ce504e25e12ad3acb47
softrain <yurimplus@naver.com>
2021-08-04 00:09:09 +0900

    Node.js + MongoDB : inflearn

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const { User } = require("./models/User");
 const config = require("./config/key");
+const { auth } = require("./middleware/auth");
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
+
 const mongoose = require("mongoose");
+const { JsonWebTokenError } = require("jsonwebtoken");
 mongoose
   .connect(config.mongoURI, {
     useNewUrlParser: true,
@@ -21,15 +26,66 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello world!"));
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   //회원가입할 때 필요한 정보를 client에서 가져오면 데이터베이스에 넣어줌
   const user = new User(req.body);
-  user.save((err, doc) => {
+  user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
     });
   });
 });
+app.post("/api/users/login", (req, res) => {
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
 
+    //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+
+      //비밀번호가 맞다면 토큰을 생성함
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        //토큰을 쿠키, 로컬스토리지에 저장
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  //여기까지 미들웨어를 통ㅌ과해 왔다는 얘기는 auth가 true라는 것
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
+  });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
